80a4d01806debd3722560b40d09e9b0c478308cb
LN27100 <heloceinlovehotmail.fr>
2024-06-11 11:58:07 +0200

    page souris ordi

--- views/ordinateur/view-mouse.php
+++ views/ordinateur/view-mouse.php
@@ -24,10 +24,64 @@
 
         <h1>Manipuler la souris</h1>
 
-        <div class="container2">
+        <div class="container">
+            <div class="row">
 
+
+                <div class="container2">
+                    <img class="imgageAccueil" src="/assets/img/souris_filaire.jpg" alt="souris filaire">
+                    <img class="imgageAccueil" src="/assets/img/souris_sans_fil.JPG" alt="souris sans fil">
+
+                    <ol>
+                        <li>Tenir la Souris Correctement</li>
+                        <ul>
+                            <li>Prise en main : Posez votre main sur la souris de manière à ce que votre paume repose légèrement sur le corps de la souris. Placez votre pouce sur le côté gauche et vos doigts (généralement l'index et le majeur) sur les boutons gauche et droit.</li>
+                            <li>Positionnement : Assurez-vous que votre bras et votre poignet sont dans une position confortable et neutre pour éviter les tensions et les douleurs.</li>
+                        </ul>
+                        <li>Mouvements de Base</li>
+                        <ul>
+                            <li>Déplacer la souris : Faites glisser la souris sur le tapis de souris pour déplacer le curseur (pointeur) à l'écran. Les mouvements de la souris sont directement reflétés par le curseur.</li>
+                            <li>Bouton gauche : Utilisé pour sélectionner, cliquer et faire glisser des éléments. Appuyez sur le bouton gauche pour cliquer.</li>
+                            <li> droit : Utilisé pour ouvrir des menus contextuels et des options supplémentaires. Un clic droit sur un élément ouvre souvent un menu avec des actions possibles.</li>
+                            <li>Molette de défilement : Située entre les boutons gauche et droit, la molette permet de faire défiler des pages verticalement (et parfois horizontalement en fonction de la souris).</li>
+                        </ul>
+
+                        <li>Types de Clics</li>
+                        <ul>
+
+                            <li>Clic gauche : Appuyez une fois sur le bouton gauche pour sélectionner ou activer un élément. Par exemple, cliquer sur une icône de programme pour le lancer.</li>
+                            <li>Double-clic : Appuyez rapidement deux fois de suite sur le bouton gauche pour ouvrir un fichier ou un dossier.</li>
+                            <li>Clic droit : Appuyez une fois sur le bouton droit pour ouvrir un menu contextuel avec des options supplémentaires.</li>
+                            <li>Clic avec la molette : Certaines souris permettent de cliquer sur la molette pour ouvrir un lien dans un nouvel onglet ou pour activer certaines fonctions spécifiques aux applications.</li>
+                        </ul>
+
+                        <li>Faire Glisser et Déposer (Drag and Drop)</li>
+
+                        <ul>
+                            <li>Sélectionner un élément : Cliquez sur un élément avec le bouton gauche et maintenez le bouton enfoncé.</li>
+                            <li>Déplacer l'élément : Faites glisser la souris tout en maintenant le bouton gauche enfoncé pour déplacer l'élément.</li>
+                            <li>Déposer l'élément : Relâchez le bouton gauche pour déposer l'élément à l'endroit souhaité.</li>
+                        </ul>
+
+                        <li>Défilement</li>
+
+                        <ul>
+                            <li>Défilement vertical : Faites rouler la molette de la souris vers l'avant ou vers l'arrière pour faire défiler une page web, un document ou une liste.</li>
+                            <li>Défilement horizontal : Certaines souris ont une fonction de défilement horizontal accessible en inclinant la molette ou en utilisant un bouton spécifique.</li>
+
+                        </ul>
+
+                    </ol>
+                </div>
+
+
+
+
+
+                <p class="lienPDF">Vous retrouverez tout cela en détail dans le fichier "bases_ordinateur" du sous-menu "Ordinateur" de l'onglet "PDF à télécharger"</p>
+
+            </div>
         </div>
-
     </main>
 
 
